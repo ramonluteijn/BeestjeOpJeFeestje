11b944b614ffd26feee1bd3e3f3828a35cb43006
ramon luteijn <ramon@intractief.nl>
2024-12-22 19:27:06 +0100

    #4 closes #4

--- BeestjeOpJeFeestje/Areas/Admin/Controllers/OrderController.cs
+++ BeestjeOpJeFeestje/Areas/Admin/Controllers/OrderController.cs
@@ -20,19 +20,4 @@ public class OrderController(OrderService orderService): Controller
         };
         return View(ordersOverviewModel);
     }
-
-    // [HttpGet("details/{id:int}")]
-    // public IActionResult Details(int id)
-    // {
-    //     var order = orderService.GetOrderById(id);
-    //     var orderViewModel = new SingleOrderViewModel()
-    //     {
-    //         Id = order.Id,
-    //         Name = order.Name,
-    //         Price = order.Price,
-    //         Type = order.Type,
-    //         Img = order.Img
-    //     };
-    //     return View(orderViewModel);
-    // }
 }
